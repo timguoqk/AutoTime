bfc620b285cf35d22819c69f8dec512e677a56c7
Tim Guo <timguoqk@users.noreply.github.com>
2015-06-22 12:20:41 -0700

    add logic, not tested yet

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,10 +3,24 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+		var entries = [
+			[1, 2, 3, 4, 5],
+			[7, 9, 11, 13, 15],
+			[8, 10, 12, 14, 16]
+		];
+		var values = [
+			["10:00 AM", "12:00 AM"],
+			["12:00 AM", "12:30 AM"],
+			["12:30 AM", "6:30 AM"]
+		];
+		for (var i = 0; i < entries.length; i ++) {
+			for (var j = 0; j < entries[i].length; j ++) {
+				$("input#startTime_" + entries[i][j]).val(values[i][0]);
+				$("input#endTime_" + entries[i][j]).val(values[i][1]);
+			}
+		}
+		for (var i = 0; i < entries[1].length; i ++)
+			$("select#type_" + entries[1][i]).val(-2);  // Meal
 
 	}
 	}, 10);
