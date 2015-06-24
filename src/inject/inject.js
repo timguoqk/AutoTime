e717a7782bcaa986c0cc2636809d6c8aeb6184bc
Tim Guo <timguoqk@users.noreply.github.com>
2015-06-23 18:17:10 -0700

    remove unnecessary boilerplate code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,6 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
 		var entries = [
 			[1, 2, 3, 4, 5],
 			[7, 9, 11, 13, 15],
@@ -21,7 +20,6 @@ chrome.extension.sendMessage({}, function(response) {
 		}
 		for (var i = 0; i < entries[1].length; i ++)
 			$("select#type_" + entries[1][i]).val(-2);  // Meal
-
 	}
 	}, 10);
 });
