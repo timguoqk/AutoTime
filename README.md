81854d421d93d5c6daffa6f99799faeb9a9b9216
Tim Guo <timguoqk@users.noreply.github.com>
2015-06-30 10:31:05 -0700

    fix bugs and add demo video

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Fill in the timesheet. (https://stratustime.centralservers.com/)
+This chrome extension with a silly name can fill in the timesheet (https://stratustime.centralservers.com/) for you automatically.
+Check demo.mp4 to see how to use it.
---
+++ src/inject.js
@@ -0,0 +1,19 @@
+var entries = [
+	[1, 2, 3, 4, 5],
+	[7, 9, 11, 13, 15],
+	[8, 10, 12, 14, 16]
+];
+var values = [
+	["10:00 AM", "12:00 PM"],
+	["12:00 PM", "12:30 PM"],
+	["12:30 PM", "6:30 PM"]
+];
+
+for (var i = 0; i < entries.length; i ++) {
+	for (var j = 0; j < entries[i].length; j ++) {
+		$("input#startTime_" + entries[i][j]).val(values[i][0]);
+		$("input#endTime_" + entries[i][j]).val(values[i][1]);
+	}
+}
+for (var i = 0; i < entries[1].length; i ++)
+	$("select#type_" + entries[1][i]).val(-2);  // Meal
